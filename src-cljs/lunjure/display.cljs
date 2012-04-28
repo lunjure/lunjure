cf4a7de5694221f56bb0854c1c4ffc1b6416d299
Moritz Ulrich <ulrich.moritz@googlemail.com>
2012-04-28 21:11:17 +0200

    use jquery awesomeness

--- src-cljs/lunjure/display.cljs
+++ src-cljs/lunjure/display.cljs
@@ -3,6 +3,21 @@
             [goog.dom :as gdom]
             [clojure.browser.event :as event]))
 
+(def jquery (js* "$"))
+
+;;; TODO
+;; (defn format-time-string [time]
+;;   (.log js/console time)
+;;   (let [date (js/Date. (* time 1000))]
+;;     (str (.getHours date) ":" (.getMinutes date))))
+(def format-time-string identity)
+
+;;; Team list (notepad)
+
+
+
+;;; Chat messages
+
 (defn append-element [el]
   (dom/append (gdom/getFirstElementChild (dom/get-element "text_window"))
               el))
@@ -11,37 +26,37 @@
 
 ;;; TODO: Change to :message
 (defmethod make-message-element :default [obj]
-  (dom/element :p {"title" (:user obj)}
-               (:text obj)))
+  (.. (jquery "<p>")
+      (attr "name" (:user obj))
+      (attr "alt" (format-time-string (:time-string obj)))
+      (text (:text obj))))
 
 (defmethod make-message-element :team [obj]
   ;; TODO: Team zur Liste hinzufuegen
-  (dom/element :p {"class" "status"}
-               (str (:user obj) " das Team " (:name obj) " erstellt.")))
+  (.. (jquery "<p>")
+      (attr "class" "status")
+      (text (str (:user obj) " das Team " (:name obj) " erstellt."))))
 
-(defmethod make-message-element :invite [obj]
-  (dom/element :p {"class" "status"}
-               (str (:user obj) " hat " (:name obj) " eingeladen.")))
-
-;;; TODO
-(defn format-time-string [time]
-  (.log js/console time)
-  (let [date (js/Date. (* time 1000))]
-    (str (.getHours date) ":" (.getMinutes date))))
+;; (defmethod make-message-element :invite [obj]
+;;   (dom/element :p {"class" "status"}
+;;                (str (:user obj) " hat " (:name obj) " eingeladen.")))
 
 (defmethod make-message-element :time [obj]
   ;; TODO: Zeit aktualisieren
-  (dom/element :p {"class" "status"}
-               (str (:user obj) " hat die Uhrzeit auf "
-                    ;; TODO: :lunch-time instead of :time
-                    (format-time-string (:time obj)) " gesetzt.")))
+  (.. (jquery "<p>")
+      (attr "class" "status")
+      (text (str (:user obj) " hat die Uhrzeit auf "
+                 ;; TODO: :lunch-time instead of :time-string
+                 (format-time-string (:time-string obj)) " gesetzt."))))
 
 (defmethod make-message-element :leave [obj]
   ;; TODO: Aus Liste entfernen
-  (dom/element :p {"class" "status"}
-               (str (:user obj) " hat das Team " (:team obj) " verlassen.")))
+  (.. (jquery "<p>")
+      (attr "class" "status")
+      (text (str (:user obj) " hat das Team " (:team obj) " verlassen."))))
 
 (defmethod make-message-element :join [obj]
   ;; TODO: Zur Liste hinzufuegen
-  (dom/element :p {"class" "status"}
-               (str (:user obj) " ist dem Team " (:team obj) " beigetreten.")))
+  (.. (jquery "<p>")
+      (attr "class" "status")
+      (text (str (:user obj) " ist dem Team " (:team obj) " beigetreten."))))
