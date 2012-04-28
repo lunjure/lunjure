581c1d632670c2db071254017ef6c5c049e27872
Moritz Ulrich <ulrich.moritz@googlemail.com>
2012-04-28 17:05:59 +0200

    server messaging works

--- src-cljs/lunjure/core.cljs
+++ src-cljs/lunjure/core.cljs
@@ -1,1 +1,19 @@
-(ns lunjure.core)
+(ns lunjure.core
+  (:require [lunjure.socket :as socket]
+            [lunjure.input :as input]
+            [clojure.browser.dom :as dom]
+            [clojure.browser.event :as event]))
+
+(event/listen (dom/get-element "message")
+              "keydown"
+              (fn [ev]
+                (when (== 13 (.-keyCode ev))
+                  (let [text (.-value (dom/get-element "message"))
+                        msg (input/parse-input text)]
+                    (socket/send-data msg)))))
+
+(let [location (.-location js/window)]
+  (socket/open-socket (str "ws://"
+                           (.-host location)
+                           (.-pathname location)
+                           "/socket")))
--- src-cljs/lunjure/socket.cljs
+++ src-cljs/lunjure/socket.cljs
@@ -1,4 +1,5 @@
-(ns lunjure.socket)
+(ns lunjure.socket
+  (:require [cljs.reader :as reader]))
 
 (def *socket* nil)
 
@@ -12,7 +13,8 @@
 (defn handle-socket-message [socket-event]
   (let [obj (reader/read-string (.-data socket-event))]
     (assert (map? obj))
-    (assert (keyword? (:type obj)))))
+    (assert (keyword? (:type obj)))
+    (.log js/console obj)))
 
 (defn ^:export open-socket [uri]
   (let [ws (if (.-MozWebSocket js/window) (js/MozWebSocket. uri) (js/WebSocket. uri))]
