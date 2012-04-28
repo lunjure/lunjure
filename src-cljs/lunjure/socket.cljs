df4844410462233d0dcd3d6138a86de767ad280c
Moritz Ulrich <ulrich.moritz@googlemail.com>
2012-04-28 11:53:37 +0200

    simple socket functions

---
+++ src-cljs/lunjure/socket.cljs
@@ -0,0 +1,22 @@
+(ns lunjure.socket)
+
+(def *socket* nil)
+
+(defn send-data [data]
+  (binding [*print-meta* true]
+    (.send *socket* (pr-str data))))
+
+(defn handle-close [])
+(defn handle-open [])
+
+(defn handle-socket-message [socket-event]
+  (let [obj (reader/read-string (.-data socket-event))]
+    (assert (map? obj))
+    (assert (keyword? (:type obj)))))
+
+(defn ^:export open-socket [uri]
+  (let [ws (if (.-MozWebSocket js/window) (js/MozWebSocket. uri) (js/WebSocket. uri))]
+    (set! (.-onopen ws)    (fn [_] (handle-open)))
+    (set! (.-onclose ws)   (fn [ ] (handle-close)))
+    (set! (.-onmessage ws) #(handle-socket-message %))
+    (set! *socket* ws)))
