8c5378ece92f0f2f93301b06303a5981c35915ed
Moritz Ulrich <ulrich.moritz@googlemail.com>
2012-04-28 12:46:05 +0200

    functions to parse input

---
+++ src-cljs/lunjure/input.cljs
@@ -0,0 +1,22 @@
+(ns lunjure.input
+  (:require [cljs.reader :as reader]
+            [lunjure.logging :as logging]))
+
+(defmulti handle-command first)
+
+(defn parse-input [text]
+  (let [obj (try
+              (reader/read-string text)
+              (catch Error e nil))]
+    (logging/log obj)
+    (if (or (nil? obj) (not (seq? obj)))
+      {:type :message
+       :text text}
+      (handle-command (map str obj) text))))
+
+(defmethod handle-command "defteam" [[_ name time alias] text]
+  {:type :defteam
+   :name name
+   :time time
+   :alias alias
+   :text text})
